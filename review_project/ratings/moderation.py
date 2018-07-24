64a5a9e196d17941211896df4f06c7a1b35a73f1
Jérémy <jeremy.lixandre@gmail.com>
2018-07-24 18:59:32 +0200

    debug keyerror commentaires

--- review_project/ratings/moderation.py
+++ review_project/ratings/moderation.py
@@ -3,6 +3,5 @@ from fluent_comments.moderation import FluentCommentsModerator
 class UserLoggedInModerator(FluentCommentsModerator):
     
     def allow(self, comment, content_object, request):
-        print('user', request.user)
         return request.user.is_authenticated
 
