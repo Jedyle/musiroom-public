c4d963222e6deae43e00132902ce83492b74eea4
Jérémy <jeremy.lixandre@gmail.com>
2018-10-07 15:01:22 +0200

    edit permissions pour commentaires

--- review_project/albums/admin.py
+++ review_project/albums/admin.py
@@ -7,10 +7,44 @@ from django.urls import reverse
 
 # Register your models here.
 
+class GenreParentFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = "parent"
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'parent'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        parents = set([genre.parent for genre in model_admin.model.objects.all()])
+        return [('aucun', 'Aucun')] + [(genre.slug, genre.name)  for genre in parents if genre is not None]
+    
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'aucun' :
+            return queryset.filter(parent__isnull = True)
+        elif self.value():
+            return queryset.filter(parent__slug = self.value())
+        else:
+            return queryset
+
 class GenreAdmin(ModerationAdmin):
     verbose_name = "Genre"
     search_fields = ['name']
-    list_filter = ('parent')
+    list_filter = [GenreParentFilter]
 
 admin.site.register(Genre, GenreAdmin)
 
