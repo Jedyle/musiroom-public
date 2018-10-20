7602dad4bc59975616ac291563f12a8750042c56
Jérémy <jeremy.lixandre@gmail.com>
2018-10-20 11:40:42 +0200

    supp notifications moderation genre

--- review_project/albums/moderator.py
+++ review_project/albums/moderator.py
@@ -1,6 +1,11 @@
 from moderation import moderation
+from moderation.moderator import GenericModerator
 from moderation.db import ModeratedModel
 
 from .models import Genre
 
-moderation.register(Genre)  # Uses default moderation settingss
+class GenreModerator(GenericModerator):
+    notify_moderator = False
+    notify_user = False
+
+moderation.register(Genre, GenreModerator)  # Uses default moderation settingss
