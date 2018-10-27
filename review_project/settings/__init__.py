d7160d5027c4ae6108786512f333342edf86fb8d
jeremy <jeremy@mindee.co>
2018-10-27 16:48:40 +0200

    debug admin inline + ajout popover sur actions et discussions

--- review_project/settings/__init__.py
+++ review_project/settings/__init__.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
 SITE_ID = 4
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
