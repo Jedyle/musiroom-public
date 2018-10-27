0dccf5d87d0a56fb8d1c4aa1710899062fea1e4c
jeremy <jeremy@mindee.co>
2018-10-27 17:17:55 +0200

    optimisation js home.html

--- review_project/settings/__init__.py
+++ review_project/settings/__init__.py
@@ -69,8 +69,8 @@ SITE_ID = 4
 
 MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
+    # 'htmlmin.middleware.HtmlMinifyMiddleware',
+    # 'htmlmin.middleware.MarkRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
