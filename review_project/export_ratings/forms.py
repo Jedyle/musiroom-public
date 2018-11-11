d45168eb6e2513d7869ea5672bdb63fa8ae89e67
jeremy <jeremy@mindee.co>
2018-11-11 22:15:49 +0100

    debug

--- review_project/export_ratings/forms.py
+++ review_project/export_ratings/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class SCExportForm(forms.Form):
-    sc_url = forms.CharField(label = "Url Senscritique", help_text="Saisissez l'url de votre profil Senscritique.")
+    sc_url = forms.CharField(label = "Url Senscritique", help_text="Saisissez l'url de votre profil Senscritique. ATTENTION : l'export ne fonctionnera que si votre profil et vos notes sont publics et visibles par tous.")
     erase = forms.BooleanField(label = "Remplacer mes notes en cas de conflit", required=False, help_text="Si vous cochez cette case, les notes des albums que vous avez déja notés sur le site seront remplacées en cas de conflit, sinon elles seront conservées.")
 
     FIELDS = (
