08fd790ad7d8d863601bd9e0935a0bbb3e0a4e73
Jérémy <jeremy.lixandre@gmail.com>
2022-04-23 18:26:17 +0200

    chg: :art: black

--- lamusitheque/lamusitheque/urls.py
+++ lamusitheque/lamusitheque/urls.py
@@ -19,13 +19,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-                  url(r'^manage/', admin.site.urls),
-                  url(r'api/', include('lamusitheque.api_routes')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    url(r"^manage/", admin.site.urls),
+    url(r"api/", include("lamusitheque.api_routes")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-                      url(r'^__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
+        url(r"^__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
