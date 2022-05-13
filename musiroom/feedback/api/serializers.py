7394ea888aa60ff57707ac70b022c3e3dbcc8659
Jedylemindee <jeremy@mindee.co>
2022-05-13 16:50:57 +0200

    new: :white_check_mark: tests for feedback app

--- musiroom/feedback/api/serializers.py
+++ musiroom/feedback/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-
 from feedback.models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
 
+    user = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    
     class Meta:
         model = Feedback
         fields = "__all__"
---
+++ musiroom/feedback/api/tests/test_views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from user_profile.tests.factories import UserFactory
+
+
+class TestFeedbackView:
+
+    URL = "/api/feedbacks/"
+
+    def test_create_feedback_anonymous(self, client):
+        data = {"type": "bug", "message": "mock mock"}
+        response = client.post(self.URL, data=data, content_type="application/json")
+        assert response.status_code == status.HTTP_201_CREATED
+        json_res = response.json()
+        assert json_res["type"] == data["type"]
+        assert json_res["message"] == data["message"]
+        assert json_res["user"] is None
+
+    def test_create_feedback_logged(self, client):
+        user = UserFactory()
+        client.force_login(user)
+        data = {"type": "bug", "message": "mock mock"}
+        response = client.post(self.URL, data=data, content_type="application/json")
+        assert response.status_code == status.HTTP_201_CREATED
+        json_res = response.json()
+        assert json_res["type"] == data["type"]
+        assert json_res["message"] == data["message"]
+        assert json_res["user"] == user.username
